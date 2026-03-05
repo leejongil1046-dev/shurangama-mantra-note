7f74f315e2b534a7056f15feec5700cc2599e094
leejongil1046-dev <leejongil1046.dev@gmail.com>
2026-03-05 19:50:00 +0900

    fix: iframe에 lazy loading 추가

--- src/component/more/youtube-video-card.tsx
+++ src/component/more/youtube-video-card.tsx
@@ -35,6 +35,7 @@ export default function YoutubeVideoCard({
           className="h-full w-full rounded-md"
           src={`https://www.youtube.com/embed/${videoId}`}
           title={title}
+          loading="lazy"
           frameBorder={0}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
